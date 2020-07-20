1f869da2f6ebc5cb1ff001f1170f135e31e05bae
Chase Tucker <tuck8106@gmail.com>
2020-07-20 14:34:13 -0700

    added username functionality

--- public/js/script.js
+++ public/js/script.js
@@ -2,38 +2,72 @@
 
 var socket = io();
 let even = true;
+let username = "";
+
+$(() => {
+  $("#username").focus();
+});
 
 $("#chat-bar").submit((e) => {
   e.preventDefault();
-  socket.emit("message", $("#message").val());
+  socket.emit("message", {
+    message: $("#message").val(),
+  });
+  // addChat(`${socket.nickname}: ${$("#message").val()}`);
   $("#message").val("");
+  $("#message").focus();
   return false;
 });
 
 socket.on("message", (msg) => {
-  console.log(`message: ${msg}`);
-  let li = $("<li>").text(msg);
-  if (even) {
-    $("#chat-list").append($("<li>").text(msg).addClass("bg-white"));
-    even = !even;
+  if (msg.username === username) {
+    addChat(`${msg.username}: ${msg.message}`, "right");
   } else {
-    $("#chat-list").append($("<li>").text(msg).addClass("bg-light"));
-    even = !even;
+    addChat(`${msg.username}: ${msg.message}`, "left");
   }
 });
-
-// socket.on("message", (msg) => {
-//   $("#chat-list").append($("<li>").text(msg));
-// });
 
 // event listener for username
 $("#login").submit((e) => {
   e.preventDefault();
-  socket.emit("username", $("#username").val());
+  username = $("#username").val();
+  $("#username-form").toggleClass("hide");
+  $("#chat-messages").toggleClass("hide");
+  $("#chat-bar-container").toggleClass("hide");
+  socket.emit("username", username);
+  $("#message").focus();
   return false;
 });
 
 // add username message to chat list
-socket.on("username", (username) => {
-  $("#chat-list").append($("<li>").text(`${username} joined the chat`));
+socket.on("username", (name) => {
+  if (name === username) {
+    addChat(`${name} joined the chat`, "right");
+  } else {
+    addChat(`${name} joined the chat`, "left");
+  }
 });
+
+function addChat(msg, orientation) {
+  if (orientation === "right") {
+    if (even) {
+      $("#chat-list").append(
+        $("<li>").text(msg).addClass("bg-white p-2 text-right")
+      );
+      even = !even;
+    } else {
+      $("#chat-list").append(
+        $("<li>").text(msg).addClass("bg-light p-2 text-right")
+      );
+      even = !even;
+    }
+  } else {
+    if (even) {
+      $("#chat-list").append($("<li>").text(msg).addClass("bg-white p-2"));
+      even = !even;
+    } else {
+      $("#chat-list").append($("<li>").text(msg).addClass("bg-light p-2"));
+      even = !even;
+    }
+  }
+}
